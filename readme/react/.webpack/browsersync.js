4567a4426fa9a6c2626753e11e871be7617e7ecd
Fabricio <fabricioviazzi@gmail.com>
2021-08-28 03:18:25 +0200

    Use config file on browsersync

--- readme/react/.webpack/browsersync.js
+++ readme/react/.webpack/browsersync.js
@@ -1,15 +1,18 @@
 // External modules
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
+// Internal modules
+const config = require("./config.json");
+
 module.exports = new BrowserSyncPlugin({
 
   host  : 'localhost',
 
-  port  : 8000,
+  port  : config.browserSync.port,
 
   proxy : 'localhost:3000',
 
-  open  : false,
+  open  : config.browserSync.open,
 
   files : [{
 
