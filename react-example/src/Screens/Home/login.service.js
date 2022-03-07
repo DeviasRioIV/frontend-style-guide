2a4fb4ea067cfb3919259ae63d877bb6925ed5af
fabricio <fabricioviazzi@gmail.com>
2022-03-07 17:19:59 +0100

    Add login service sample

---
+++ react-example/src/Screens/Home/login.service.js
@@ -0,0 +1,31 @@
+export default async function loginService (body) {
+
+  const requestOptions = {
+    mathod: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(body)
+  }
+
+  // Default response
+  let success = false
+  let data = { error: 'There was an issue with your request' }
+
+  const response = await fetch(`login`, requestOptions)
+
+  const json = response.json()
+
+  if (response.status === 200) {
+    success = true
+    data = json
+  } else if (json.error) {
+    data.error = json.error
+  }
+
+  return {
+    success: success,
+    data: data
+  }
+}
